0269a2773546ad2a482e8f332d03cf3c5437a605
Chris <chrishill04@gmail.com>
2023-12-14 22:41:05 -0500

    B1 Complete:  Creates variable in app component to read API and display Eng and Fr welcome messages on HTML page

--- src/main/java/edu/wgu/d387_sample_code/controller/GreetingController.java
+++ src/main/java/edu/wgu/d387_sample_code/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package edu.wgu.d387_sample_code.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GreetingController {
 
     private static ArrayList<String> greetings = new ArrayList<String>();
