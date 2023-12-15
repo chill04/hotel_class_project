11563b678d699d5d28d99fa32954236117bf0d8f
Chris <chrishill04@gmail.com>
2023-12-15 15:57:06 -0500

    C1:  creates Dockerfile

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -15,6 +15,7 @@ import java.util.Locale;
 public class D387SampleCodeApplication implements CommandLineRunner {
 	@Autowired
 	MessageSource messageSource;
+
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
 	}
@@ -27,8 +28,9 @@ public class D387SampleCodeApplication implements CommandLineRunner {
 		GreetingTranslationService englishTranslationService = new GreetingTranslationService();
 		GreetingTranslationService frenchTranslationService = new GreetingTranslationService();
 		// Call each thread
-		String englishWelcome = englishTranslationService.createWelcomeMessage(Locale.CANADA, messageSource);
+
 		String frenchWelcome = frenchTranslationService.createWelcomeMessage(Locale.CANADA_FRENCH, messageSource);
+		String englishWelcome = englishTranslationService.createWelcomeMessage(Locale.CANADA, messageSource);
 		// Add threads to Controller
 		greetingController.addGreeting(englishWelcome);
 		greetingController.addGreeting(frenchWelcome);
